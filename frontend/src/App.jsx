73d2fab0769616d53dba899c315c39551bee67c6
Atul Tingre <atultingre.work@gmail.com>
2026-04-21 14:59:44 +0530

    Initial Routes Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import ManageUsers from "./pages/Admin/ManageUsers";
+import CreateTask from "./pages/Admin/CreateTask";
+
+import UserDashboard from "./pages/User/UserDashboard";
+import MyTasks from "./pages/User/MyTasks";
+import ViewTaskDetails from "./pages/User/ViewTaskDetails.jsx";
+
+import PrivateRoute from "./routes/PriveRoute";
 
 const App = () => {
-  return <div className="">Atul Tingre</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Admin */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/tasks" element={<ManageTasks />} />
+          <Route path="/admin/create-task" element={<CreateTask />} />
+          <Route path="/admin/users" element={<ManageUsers />} />
+        </Route>
+
+        {/* Users */}
+        <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+          <Route path="/user/tasks" element={<MyTasks />} />
+          <Route path="/user/tasks-details/:id" element={<ViewTaskDetails />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
