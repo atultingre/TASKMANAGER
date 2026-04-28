ce860cbabbe68e549d53f356b703741ea6620437
Atul Tingre <atultingre.work@gmail.com>
2026-04-28 17:22:25 +0530

    Backend API Paths

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,5 @@ app.use("/api/reports", reportRoutes);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
