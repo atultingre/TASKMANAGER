832495fa6e76fdcf4af12d9bce8c85c2e01c29fc
Atul Tingre <atultingre.work@gmail.com>
2026-07-13 02:32:55 +0530

    Sidebar menu

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../context/userContext";
+import { useNavigate } from "react-router-dom";
+import { SIDE_MENU_DATA, SIDE_MENU_USER_DATA } from "../../utils/data";
 
-const SideMenu = () => {
-  return <div>SideMenu</div>;
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const [sideMenuData, setSideMenuData] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if (user) {
+      setSideMenuData(
+        user.role === "admin" ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA,
+      );
+    }
+    return () => {};
+  }, [user]);
+
+  return (
+    <div className="w-64 h-calc(100vh-61px) bg-white border-r border-gray-200/50 backdrop-blur-[2px] sticky top-[61px] z-20 ">
+      <div className="flex flex-col items-center justify-center mb-7 pt-5">
+        <div className="relative mb-5 bg-gray-100 p-5 rounded-xl">
+          <img
+            src={user?.profileImageUrl || ""}
+            alt="Profile Image"
+            className="w-20 h-20 bg-slate-400 rounded-full mx-auto object-cover"
+          />
+          {user?.role === "admin" && (
+            <div className="text-[10px] w-20 m-auto text-center font-medium text-white bg-primary px-3 py-0.5 rounded mt-1">
+              Admin
+            </div>
+          )}
+
+          <h5 className="text-gray-950 text-center font-medium leading-6 mt-3">
+            {user?.name || ""}
+          </h5>
+
+          <p className="text-[12px] text-gray-500">{user?.email || ""}</p>
+        </div>
+        {sideMenuData.map((item, index) => (
+          <button
+            key={`menu_${index}`}
+            className={`w-full flex items-center gap-4 text-[15px] side-menu-item ${activeMenu === item.label ? "text-primary bg-linear-to-r from-blue-50/40 to-blue-100/50 border-r-3" : ""} py-3 px-6 mb-3 cursor-pointer`}
+            onClick={() => handleClick(item.path)}
+          >
+            <item.icon className="w-5 h-5" />
+            <span>{item.label}</span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SideMenu;
