83cacfe7e1bd6a7aa69e56c8e62be76fefd01c1f
Atul Tingre <atultingre.work@gmail.com>
2026-07-13 02:34:11 +0530

    Dashboard Tasks Stats

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,15 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
 import { useContext } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
+import InfoCard from "../../components/Cards/InfoCard";
+import { addThousandSeparator } from "../../utils/helper";
 
 const Dashboard = ({ children, activeMenu }) => {
   useUserAuth(); // This will handle authentication and redirection if not authenticated
 
   const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
 
-  return <DashboardLayout>Dashboard</DashboardLayout>;
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA,
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+    return () => {};
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="card my-5">
+        <div className="">
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+            <InfoCard
+              label="Total Tasks"
+              value={addThousandSeparator(
+                dashboardData?.charts?.taskDistribution?.All || 0,
+              )}
+              color="bg-primary"
+            />
+            <InfoCard
+              label="Pending Tasks"
+              value={addThousandSeparator(
+                dashboardData?.charts?.taskDistribution?.Pending || 0,
+              )}
+              color="bg-violet-500"
+            />
+            <InfoCard
+              label="In Progress Tasks"
+              value={addThousandSeparator(
+                dashboardData?.charts?.taskDistribution?.InProgress || 0,
+              )}
+              color="bg-cyan-500"
+            />
+            <InfoCard
+              label="Completed Tasks"
+              value={addThousandSeparator(
+                dashboardData?.charts?.taskDistribution?.Completed || 0,
+              )}
+              color="bg-lime-500"
+            />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
